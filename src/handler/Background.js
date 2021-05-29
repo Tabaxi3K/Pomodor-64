e1d5aa484f987ad2cb29fad305e727e819293468
Tabaxi3K <62475447+Tabaxi3K@users.noreply.github.com>
2021-05-29 13:45:51 -0400

    Finished Background.js

--- src/handler/Background.js
+++ src/handler/Background.js
@@ -15,8 +15,29 @@ export const changeBg = (event) => {
             break;
 
         case 'short-break': //if you click the short break button
-            target.className = 'time-btn-active';
-            active(event.currentTarget);
-            document.getElementsByClassName('wrapper')[0].style.background = 'rgb(76, 166, 169)';
+            target.className = 'time-btn-active'; //where you clicked
+            active(event.currentTarget); //make current button active
+            document.getElementsByClassName('wrapper')[0].style.background = 'rgb(76, 166, 169)'; //change page background
+            document.getElementsByClassName('time-count-btn')[0].style.color = 'rgb(76, 166, 169)'; //make button active
+            document.getElementsByClassName('time-count')[0].innerHTML = '05:00';
+            document.getElementsByClassName('start-work')[0].innerHTML = 'Time for a break'; //change state of plan
+            break;
+
+        case 'long-break': //if you click the long break button
+            target.className = 'time-btn-active'; //where you clicked
+            active(event.currentTarget); //make current button acitve
+            document.getElementsByClassName('wrapper')[0].style.background = 'rgb(73, 143, 193)'; //change page background
+            document.getElementsByClassName('time-count-btn')[0].style.color = 'rgb(73, 143, 193)'; //make button active
+            document.getElementsByClassName('time-count')[0].innerHTML = '15:00';
+            document.getElementsByClassName('start-work')[0].innerHTML = 'Time for a break'; //change state of plan
+    }
+
+    function active(parent){
+        Array.from(parent.children).forEach((element) => {
+            //make unclicked buttons inactive
+            if(element !== event.target){
+                element.className = 'time-btn';
+            }
+        });
     }
 };
